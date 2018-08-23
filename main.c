13abef842babd3db5cc5681e60e360e0e025f44e
Architector120 <42624233+Architector120@users.noreply.github.com>
2018-08-23 14:50:28 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+ * ATtiny13A_Morse_Code_Transmitter.c
+ *
+ * Created: 22.08.2018 11:45:27
+ * Author : Architector120
+ * Email  : architector120@cantseeme.xyz
+ */ 
+
+#define  F_CPU 1200000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include "morse_code.h"
+
+int main(void)
+{
+	morse_init();
+    while (1) 
+    {
+		morse_puts("Hello, world!");
+		_delay_ms(3000);
+    }
+}
+
---
+++ morse_code.h
@@ -0,0 +1,41 @@
+/*
+ * morse_code.h
+ *
+ * Created: 22.08.2018 11:45:27
+ * Author : Architector120
+ * Email  : architector120@cantseeme.xyz
+ */
+
+#ifndef MORSE_CODE_H_
+#define MORSE_CODE_H_
+
+//=====================================================
+//		Setup output pin.
+//=====================================================
+#define MORSE_DDR			DDRB
+#define MORSE_PORT			PORTB
+#define MORSE_PIN			PINB0
+
+//=====================================================
+//		Morse code timing.
+//=====================================================
+#define morse_dot_time 		100
+#define morse_dash_time 	280
+#define morse_next_b_time 	100
+#define morse_next_l_time 	280		
+#define morse_space_time 	650		
+
+//=====================================================
+//		Functions.
+//=====================================================
+#define morse_init(void) MORSE_DDR |= (1 << MORSE_PIN);
+extern void morse_transmit(char num, char data);
+extern void morse_puts(char *puts);
+extern void morse_putc(char putc);
+
+//=====================================================
+//		ASCII to Morse Code Conversion Array.
+//=====================================================
+extern const char ascii_to_morse[95][2];
+
+#endif /* MORSE_CODE_H_ */
